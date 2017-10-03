7b8c80da8e68de21f9292701f91f7836624c6dab
vimalvk9 <kumarvimalvk9@gmail.com>
2017-10-03 15:09:18 +0530

    Done

--- STPAR.cpp
+++ STPAR.cpp
@@ -1,55 +1,51 @@
-#include<iostream>
-#include<stdio.h>
-#include<vector>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
-    while(1)
-    {
-        int t,n=1;
-        scanf("%d",&t);
-        if(t==0)
-        {
-            return 0;
-        }
-        int a[t],i;
-        for(i=0;i<t;i++)
-        {
-            scanf("%d",&a[i]);
-        }
-        vector <int> dest(0);
-        vector <int> side(0);
-        for(i=0;i<t;i++)
-        {
-            if(a[i]==n)
-            {
-                dest.push_back(n);
-                n++;
-                a[i]=0;
-            }
-            else if(side.size()>0 && side.back()==n)
-            {
-                    dest.push_back(n);
-                    side.pop_back();
-                    n++;
-                    i--;
-            }
-            else if(a[i]!=0)
-            {
-                if(side.size()>0)
-                {
-                    if(a[i]>side.back())
-                    {
-                        printf("no\n");
-                        goto s;
-                    }
-                }
-                side.push_back(a[i]);
-                a[i]=0;
-            }
-        }
-        printf("yes\n");
-        s:{}
-    }
+    int n , x ;
+    cin >> n;
+
+    while(n)
+     {
+     	bool flag = 1;
+     	stack <int> st;
+     	vector <int> v;
+
+     	for(int i=0;i<n;i++)
+     	{
+     		cin >> x;
+
+     		while(st.size() && st.top() < x)
+     		{
+     			v.push_back(st.top());
+     			st.pop();
+     		}
+
+     		st.push(x);
+     	}
+
+     	while(!st.empty())
+     	{
+     		v.push_back(st.top());
+     		st.pop();
+     	}
+
+     	for(int i=0;i<n;i++)
+     	{
+     		if(v[i]>v[i+1])
+             {
+             	flag = 0;
+             	break;
+             }     			
+     	}
+
+     	if(flag)
+     		cout << "yes\n";
+     	else
+     		cout << "no\n";
+
+     	cin >> n;
+     }
+    
     return 0;
 }
